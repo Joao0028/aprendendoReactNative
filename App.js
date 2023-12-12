c32711e7d3265ed972551755c89bfdf468c3a0bb
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-12 07:12:13 -0300

    Aprendendo a estilizar

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  
-  const [ nome, setNome ] = useState("João Paulo Silva")
+  const [nome, setNome] = useState("João Paulo Silva");
 
-  function entrar(){
-    return setNome("Seja bem-vindo!!!")
+  function entrar() {
+    return setNome("Seja bem-vindo!!!");
   }
-  
+
   return (
     <View style={styles.container}>
-      <Text style={{textAlign: 'center', margin: 15, color: "#212121", fontSize: 18}}>{nome}</Text>
-      <Button title='Entrar' onPress={entrar}/>
-      
+      <Text style={styles.nome}>{nome}</Text>
+      <Button title="Entrar" onPress={entrar} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {},
+  nome: {
+    textAlign: "center",
+    margin: 15,
+    color: "#313131",
+    fontSize: 18,
   },
 });

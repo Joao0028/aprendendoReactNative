456d7fc9c495c20a5805f23fd164e68006253a73
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-13 06:50:24 -0300

    Criando um app de biscoito da sorte

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import BiscoitoDaSorte from "./src/components/BiscoitoDaSorte";
 
 export default function App() {
-  const [ conteudoInput, setInput ] = useState("")
-
-  if(conteudoInput === ""){
-    return setInput("Seja bem-vindo!!!")
-  }
-
-  function pegaNome(nome){
-    return setInput("Seja bem-vindo " + nome + "!")
-  }
-
   return (
     <View style={styles.container}>
-        <TextInput 
-        placeholder="Digite seu nome"
-        onChangeText={(texto) => pegaNome(texto)}
-        underlineColorAndroid="transparent"
-        style={styles.input}
-        />
-
-        <Text style={styles.texto}>
-          {conteudoInput}
-        </Text>
+       <BiscoitoDaSorte />
     </View>
   );
 }
@@ -32,19 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 30
   },
-  input: {
-    height: 45,
-    borderWidth: 1,
-    borderColor: "#222",
-    margin: 18,
-    fontSize: 20,
-    padding: 10,
-    borderRadius: 20,
-    paddingLeft: 20
-    },
-    texto: {
-      textAlign: "center",
-      fontSize: 18
-    }
   })

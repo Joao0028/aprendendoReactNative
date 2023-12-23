5a98318a114ea27c79f7554f6e60984f77409c28
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-23 13:09:13 -0300

    Criando loader

--- src/components/RequisicoesHTTPAxios/index.js
+++ src/components/RequisicoesHTTPAxios/index.js
@@ -5,36 +5,40 @@ import Filme from "./Filme";
 
 export default function RequisicoesHTTPAxios() {
   const [arrayApi, setArray] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function req() {
-        try {
-            const response = await api.get("r-api/?api=filmes");
-            setArray(response.data); // O data é onde fica o conteúdo
-          } catch (error) {
-            console.error("Erro na requisição:", error);
-          }
+      try {
+        const response = await api.get("r-api/?api=filmes");
+        setArray(response.data); // O data é onde fica o conteúdo
+        setLoading(false);
+      } catch (error) {
+        console.error("Erro na requisição:", error);
+      }
     }
     req();
-  },[]);
+  }, []);
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={arrayApi}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => <Filme props={item}/>}
-      />
-    </View>
-  );
+  if (loading === true) {
+    return (
+      <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
+        <Text style={{fontSize: 20, fontWeight: "bold"}}>Carregando...</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={arrayApi}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Filme props={item} />}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {},
 });
-
-
-// {return(<View>
-//     <Text>{item.nome}</Text>
-//     <Image style={{width: 200, height: 100}} source={{uri: item.foto}} />
-// </View>)}

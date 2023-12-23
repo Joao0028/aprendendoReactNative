088f8fd52f2cdaf1cb87cf0c78b41c937ef5115d
Joao0028 <joaopaulonascimento720@gmail.com>
2023-12-23 12:49:42 -0300

    Criando componente para os filmes da API

--- src/components/RequisicoesHTTPAxios/Filme/index.js
+++ src/components/RequisicoesHTTPAxios/Filme/index.js
@@ -1,11 +1,27 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function Filme({props}){
-    return <View>
-        <Text>{props.nome}</Text>
+    return <View style={estilos.container}>
+        <Text style={estilos.titulo}>{props.nome}</Text>
         
-        <Image style={{width: 200, height: 100}} source={{uri: props.foto}} />
+        <Image style={{width: "100%", height: 200, borderRadius: 18}} source={{uri: props.foto}} />
 
-        <Text>{props.sinopse}</Text>
+        <Text style={estilos.sinopse}>{props.sinopse}</Text>
     </View>
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 20,
+        gap: 8
+    },
+    titulo:{
+        fontWeight: "bold",
+        fontSize: 20
+    },
+    sinopse:{
+        fontSize: 12,
+        textAlign: "justify",
+    }
+})
